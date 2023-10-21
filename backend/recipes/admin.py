fa0e93ac99091741312f6bdf3ed240de09a41914
Сергей Тыртычный <tyrtychnyy90@mail.ru>
2023-10-21 17:49:53 +0300

    Models_adminka

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -2,95 +2,56 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Category, Comment, Genre, Review, Title
+from recipes.models import Ingredient, Recipe, Tag
 
 User = get_user_model()
 
 
-@admin.register(User)
+@admin.register(User, UserAdmin)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
         'username',
         'email',
-        'role',
         'first_name',
         'last_name',
-        'bio'
     )
     list_editable = (
+        'username',
         'email',
-        'role',
         'first_name',
         'last_name',
-        'bio'
     )
-    search_fields = ('username',)
-    list_filter = ('username',)
+    search_fields = ('username')
+    list_filter = ('username', 'email')
 
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
     list_display = (
-        'pk',
         'name',
-        'slug'
+        'measurement_unit',
     )
-    search_fields = ('name', 'slug')
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'author',
+    )
+    search_fields = ('name', 'author', 'slug')
     list_filter = ('name', 'slug')
     empty_value_display = '-пусто-'
 
 
-@admin.register(Genre)
-class GenreAdmin(admin.ModelAdmin):
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     list_display = (
-        'pk',
         'name',
-        'slug'
+        'slug',
+        'color',
     )
-    search_fields = ('name', 'slug')
-    list_filter = ('name', 'slug')
-    empty_value_display = '-пусто-'
-
-
-@admin.register(Title)
-class TitleAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'name',
-        'year',
-        'description',
-        'category',
-    )
-    search_fields = ('name', 'year', 'category')
-    list_filter = ('category', 'year')
-    empty_value_display = '-пусто-'
-    filter_horizontal = ['genre']
-
-
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'title',
-        'text',
-        'author',
-        'pub_date',
-        'score',
-    )
-    search_fields = ('name', 'year', 'category')
-    list_filter = ('author', 'score')
-    empty_value_display = '-пусто-'
-
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'author',
-        'pub_date',
-        'text',
-        'review',
-    )
-    search_fields = ('author', 'review', )
-    list_filter = ('author',)
-    empty_value_display = '-пусто-'
+    search_fields = ('name',)
+    list_filter = ('name',)
