623f47dc74fb75bed4dee040f32599b245feca62
Сергей Тыртычный <tyrtychnyy90@mail.ru>
2023-11-04 00:29:43 +0300

    ewew

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,12 +1,36 @@
+from django.contrib.auth import get_user_model
 from django_filters import rest_framework as filters
 
 from recipes.models import Recipe
 
+User = get_user_model()
+
+# class RecipeFilters(filters.FilterSet):
+#     """Класс фильтров полей модели Title"""
+
+#     tags = filters.CharFilter(field_name='tags__slug')
+
+#     class Meta:
+#         model = Recipe
+#         fields = ('tags', 'author')
+
 
 class RecipeFilters(filters.FilterSet):
-    """Класс фильтров полей модели Title"""
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart')
 
-    tags = filters.CharFilter(field_name='tags__slug')
+    def filter_is_favorited(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(favorites__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(cart__user=self.request.user)
+        return queryset
 
     class Meta:
         model = Recipe
