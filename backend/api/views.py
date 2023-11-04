b6a088c3f00edb134df8c7ae6238ce3d71bf6d18
Сергей Тыртычный <tyrtychnyy90@mail.ru>
2023-11-04 23:54:02 +0300

    OKK

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,11 +7,11 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+
 from recipes.models import (BuyRecipe, FavoriteRecipe,
                             Ingredient, IngredientRecipe,
                             Recipe, Tag)
 from users.models import Follow
-
 from .filters import RecipeFilters
 from .permissions import IsAdminOrReadOnly, IsAuthorOrAdminOrReadOnly
 from .serializers import (FollowSerializer, IngredientSerializer,
